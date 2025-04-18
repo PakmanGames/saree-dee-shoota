2b6ba2e3cb3cce55498075dfc0ec99f178e21bfd
Andy Pak <im.andy.pak@gmail.com>
2025-04-18 11:11:38 -0400

    Testing

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,23 @@
 import { useGLTF } from "@react-three/drei";
-import { useEffect } from "react";
 import { RigidBody } from "@react-three/rapier";
+import { useEffect } from "react";
 
 export const Map = () => {
-    const map = useGLTF("models/map.glb");
-    
-    useEffect(() => {
-        map.scene.traverse((child) => {
-            if (child.isMesh) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-            }
-        });
+  const map = useGLTF("models/map.glb");
+  useEffect(() => {
+    map.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
     });
-
-    return (
-        <RigidBody colliders="trimesh" type="fixed">
-            <primitive object={map.scene} />
-        </RigidBody>
-    );
-}
+  });
+  return (
+    <>
+      <RigidBody colliders="trimesh" type="fixed">
+        <primitive object={map.scene} />
+      </RigidBody>
+    </>
+  );
+};
 useGLTF.preload("models/map.glb");
