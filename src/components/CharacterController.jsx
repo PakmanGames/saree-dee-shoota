98d56be37363c93fbd396897c8f302ffe9721284
Andy Pak <im.andy.pak@gmail.com>
2025-04-18 11:05:08 -0400

    Add character movements but it doesn't work :sob:

--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -1,6 +1,9 @@
-import { useRef, useState, useEffect } from "react"
+import { useRef, useState } from "react"
 import { CharacterSoldier } from "./CharacterSoldier"
 import { RigidBody, CapsuleCollider } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+
+const MOVEMENT_SPEED = 200;
 
 export const CharacterController = ({
     state,
@@ -13,17 +16,50 @@ export const CharacterController = ({
     const rigidbody = useRef();
     const [animation, setAnimation] = useState("Idle");
     
-    useEffect(() => {
-        console.log("CharacterController rendered:", {
-            stateId: state.id,
-            color: state.state.profile?.color,
-            userPlayer
-        });
-    }, [state, userPlayer]);
+    // useEffect(() => {
+    //     console.log("CharacterController rendered:", {
+    //         stateId: state.id,
+    //         color: state.state.profile?.color,
+    //         userPlayer
+    //     });
+    // }, [state, userPlayer]);
+
+    useFrame((_, delta) => {
+        const angle = joystick.angle; // Get angle from joystick to update player position
+        if (joystick.isJoystickPressed() && angle) {
+            setAnimation("Run");
+            character.current.rotation.y = angle;
+
+            const impulse = {
+                x: Math.sin(angle) * MOVEMENT_SPEED * delta,
+                y: 0,
+                z: Math.cos(angle) * MOVEMENT_SPEED * delta,
+            };
+    
+            rigidbody.current.applyImpulse(impulse, true);
+        } else {
+            setAnimation("Idle");
+        }
+
+        if (isHost()) {
+            state.setState("pos", rigidbody.current.translation());
+        } else {
+            const pos = state.getState("pos");
+            if (pos) {
+                rigidbody.current.setTranslation(pos);
+            }
+        }
+    });
 
     return (
         <group ref={group} {...props}>
-            <RigidBody ref={rigidbody} colliders={false}>
+            <RigidBody 
+                ref={rigidbody} 
+                colliders={false} 
+                linearDamping={12} 
+                lockRotations
+                type={isHost() ? "dynamic" : "kinematicPosition"}
+            >
                 <group ref={character}>
                     <CharacterSoldier
                         color={state.state.profile?.color || "red"}
