a23be6e60a899c9493572e15bb5d4cfc22f1edb1
Andy Pak <im.andy.pak@gmail.com>
2025-04-19 11:03:14 -0400

    Create a bullet component

---
+++ src/components/Bullet.jsx
@@ -0,0 +1,35 @@
+import { RigidBody } from "@react-three/rapier";
+import { useEffect, useRef } from "react";
+
+const BULLET_SPEED = 20;
+
+const bulletMaterial = new MeshBasicMaterial({
+    color: "hotpink",
+    toneMapped: false,
+});
+
+bulletMaterial.color.multiplyScalar(42);
+
+export const Bullet = ({ player, angle, position, onHit }) => {
+    const rigidbody = useRef();
+
+    useEffect(() => {
+        const velocity = {
+            x: Math.sin(angle) * BULLET_SPEED,
+            y: 0,
+            z: Math.cos(angle) * BULLET_SPEED,
+        };
+
+        rigidbody.current.setLinvel(velocity, true);
+    }, []);
+
+    return (
+        <group position={[position.x, position.y, position.z]} rotation-y={angle}>
+            <RigidBody ref={rigidbody}>
+                <mesh position-z={0.25} material={bulletMaterial} castShadow>
+                    <boxGeometry args={[0.05, 0.05, 0.5]} />
+                </mesh>
+            </RigidBody>
+        </group>
+    );
+}
