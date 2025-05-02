0f0364bbb22940818260a0496ad33562bc761adf
Andy Pak <im.andy.pak@gmail.com>
2025-05-02 14:06:10 -0400

    Add postprocessing effect when bullet is shot

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Experience } from "./components/Experience";
 import { SoftShadows } from "@react-three/drei";
 import { Suspense } from "react";
 import { Physics } from "@react-three/rapier";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           <Experience />
         </Physics>
       </Suspense>
+      <EffectComposer disableNormalPass>
+        <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+      </EffectComposer>
     </Canvas>
   );
 }
